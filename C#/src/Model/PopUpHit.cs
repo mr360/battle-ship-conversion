7b96af01969671076880335e98275958f658e8c1
Niloofarist <37201854+Niloofarist@users.noreply.github.com>
2018-04-20 00:35:12 +1000

    Update and rename PopUpAnimation.cs to PopUpHit.cs

---
+++ C#/src/Model/PopUpHit.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SwinGameSDK;
+
+public class PopUpHit : ResultOfAttack
+{
+
+  public PopupHit PopupHit { get; set; }
+
+  myPopup.AllowsTransparency = true;
+  
+  public event PopupEventHandler Popup
+  {
+    private void ToolTip1_Popup(Object sender, PopupEventArgs e) 
+    {
+    System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+    messageBoxCS.AppendFormat("{0} = {1}", "Hit", e.Hit );
+    messageBoxCS.AppendLine();
+    MessageBox.Show(messageBoxCS.ToString(), "Popup Event" );
+    }
+  }
+  
+  myTextBlockPopup.PopupHit = PopupHit.Fade;
+  
+  }
+
